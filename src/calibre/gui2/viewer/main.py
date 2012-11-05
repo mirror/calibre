2c3180d613084f4bb0bf8b7a5f02bb6f93c21d2b
John Peterson <john.peterson3@hotmail.com>
2012-11-05 06:00:05 +0000

    Response to merge review.
    1) Fixed by making it optional.
    
    1.5) Fixed by making it optional.
    
    2) Fixed.
    
    4) Fixed.
    
    5) Fixed.

--- src/calibre/gui2/viewer/main.py
+++ src/calibre/gui2/viewer/main.py
@@ -274,11 +274,36 @@ class EbookViewer(MainWindow, Ui_EbookViewer):
         self.tool_bar2.setContextMenuPolicy(Qt.PreventContextMenu)
         self.tool_bar.widgetForAction(self.action_bookmark).setPopupMode(QToolButton.MenuButtonPopup)
         self.action_full_screen.setCheckable(True)
+        self.full_screen_label = QLabel('''
+                <center>
+                <h1>%s</h1>
+                <h3>%s</h3>
+                <h3>%s</h3>
+                <h3>%s</h3>
+                </center>
+                '''%(_('Full screen mode'),
+                    _('Right click to show controls'),
+                    _('Tap in the left or right page margin to turn pages'),
+                    _('Press Esc to quit')),
+                    self)
+        self.full_screen_label.setVisible(False)
+        self.full_screen_label.setStyleSheet('''
+        QLabel {
+            text-align: center;
+            background-color: white;
+            color: black;
+            border-width: 1px;
+            border-style: solid;
+            border-radius: 20px;
+        }
+        ''')
         self.window_mode_changed = None
         self.toggle_toolbar_action = QAction(_('Show/hide controls'), self)
         self.toggle_toolbar_action.setCheckable(True)
         self.toggle_toolbar_action.triggered.connect(self.toggle_toolbars)
         self.addAction(self.toggle_toolbar_action)
+        self.full_screen_label_anim = QPropertyAnimation(
+                self.full_screen_label, 'size')
         self.clock_label = QLabel('99:99', self)
         self.clock_label.setVisible(False)
         self.clock_label.setFocusPolicy(Qt.NoFocus)
@@ -300,7 +325,7 @@ class EbookViewer(MainWindow, Ui_EbookViewer):
         self.pos_label.setFocusPolicy(Qt.NoFocus)
         self.clock_timer = QTimer(self)
         self.clock_timer.timeout.connect(self.update_clock)
-
+        
         self.print_menu = QMenu()
         self.print_menu.addAction(QIcon(I('print-preview.png')), _('Print Preview'))
         self.action_print.setMenu(self.print_menu)
@@ -425,7 +450,7 @@ class EbookViewer(MainWindow, Ui_EbookViewer):
         self.toggle_paged_mode(self.action_toggle_paged_mode.isChecked(),
                 at_start=True)
         fullscreen = vprefs.get('fullscreen', None)
-        if fullscreen:
+        if fullscreen and self.view.document.fullscreen_save_state:
             self.showFullScreen()
 
     def lookup(self, word):
@@ -557,6 +582,7 @@ class EbookViewer(MainWindow, Ui_EbookViewer):
         self.action_full_screen.setChecked(False)
         self.tool_bar.setVisible(True)
         self.tool_bar2.setVisible(True)
+        self.full_screen_label.setVisible(False)
         if hasattr(self, '_original_frame_margins'):
             om = self._original_frame_margins
             self.centralwidget.layout().setContentsMargins(om[0])
